ac105eaacf494d2ad0116a1fc3e0feaac863cf46
Ahmed Mohamed <am0103738@gmail.com>
2024-03-16 16:47:04 +0200

    Add event listener to open links in new tab

--- popup.js
+++ popup.js
@@ -293,3 +293,16 @@ function checkQuestionHeight(doc, y, lineHeight, remainingHeight, question) {
   }
   return true;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var links = document.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+    (function () {
+      var ln = links[i];
+      var location = ln.href;
+      ln.onclick = function () {
+        chrome.tabs.create({ active: true, url: location });
+      };
+    })();
+  }
+});
